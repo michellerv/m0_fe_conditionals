dc899e36028cc72f6f4f71732e86fa00ffab575d
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-19 14:44:28 -0600

    Add first attempt condititonals

--- codeChallenges/medium.js
+++ codeChallenges/medium.js
@@ -13,3 +13,17 @@
 //  they should pay full price
 // - If the user is not over 25 and has a bad driving record, 
 // they need to have someone else sign for the rental
+
+var goodDrivingRecord = true;
+var age = 24; 
+
+if (goodDrivingRecord = true && age > 25){
+    console.log("car rental discount");
+}  
+    else if (goodDrivingRecord = true || > 25) {
+        console.log ("Pay full price")
+}
+
+    else if (goodDrivingRecord !== true && age < 25) {
+        console.log ("Someone else sign for rental")
+    }
